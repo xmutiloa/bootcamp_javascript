d61cfd0aa3065dec190f4abe14c2159332e11405
Muti <xmutiloa@hotmail.com>
2020-06-16 17:28:01 +0200

    Modulo 8. Imports

---
+++ Imports/js/client-list-business.js
@@ -0,0 +1,14 @@
+import * as DataBusiness from "./data-business";
+import * as ClientBusiness from "./client-business";
+
+function printClientsAccounts() {
+    const clients = DataBusiness.getClients();
+    const ul = document.createElement("ul");
+    for (let client of clients) {
+        const element = ClientBusiness.getClientElement(client);
+        ul.appendChild(element);
+    }
+    document.getElementById("root").appendChild(ul);
+}
+
+export { printClientsAccounts };
